0daabeb67e3045cecebfd95c6cf0780cee289de6
MathewTheCoder <jmathew1201@live.com>
2016-06-09 21:27:56 +0530

    Added intial files

---
+++ js/app.js
@@ -0,0 +1,357 @@
+$(document).ready(function(){
+
+	
+	$(".color1").on("click", function(){
+        
+		$(this).removeClass("tile1").addClass('lock');
+		$(".color1").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});	 
+
+		
+		$(".wrong").click(function(){
+	    	    	
+	    	$(".color1").off("click");
+
+		});
+		
+	 
+	});
+    
+	  
+	
+	
+	$(".color2").on("click", function(){
+        
+		$(this).removeClass("tile2").addClass('lock');
+		$(".color2").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});	 
+
+		
+		$(".wrong").click(function(){
+	    	    	
+	    	$(".color2").off("click");
+
+		});
+		
+	 
+	});
+
+	$(".color3").on("click", function(){
+        
+		$(this).removeClass("tile3").addClass('lock');
+		$(".color3").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});	 
+
+		
+		$(".wrong").click(function(){
+	    	    	
+	    	$(".color3").off("click");
+
+		});
+		
+	 
+	});
+
+	$(".color4").on("click", function(){
+        
+		$(this).removeClass("tile4").addClass('lock');
+		$(".color4").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});	 
+
+		
+		$(".wrong").click(function(){
+	    	    	
+	    	$(".color4").off("click");
+
+		});
+		
+	 
+	});
+
+	$(".color5").on("click", function(){
+        
+		$(this).removeClass("tile5").addClass('lock');
+		$(".color5").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});	 
+
+		
+		$(".wrong").click(function(){
+	    	    	
+	    	$(".color5").off("click");
+
+		});
+		
+	 
+	});
+
+	$(".color6").on("click", function(){
+        
+		$(this).removeClass("tile6").addClass('lock');
+		$(".color6").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});	 
+
+		
+		$(".wrong").click(function(){
+	    	    	
+	    	$(".color6").off("click");
+
+		});
+		
+	 
+	});
+
+	$(".color7").on("click", function(){
+        
+		$(this).removeClass("tile7").addClass('lock');
+		$(".color7").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});	 
+
+		
+		$(".wrong").click(function(){
+	    	    	
+	    	$(".color7").off("click");
+
+		});
+		
+	 
+	});
+
+	$(".color8").on("click", function(){
+        
+		$(this).removeClass("tile8").addClass('lock');
+		$(".color8").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});	 
+
+		
+		$(".wrong").click(function(){
+	    	    	
+	    	$(".color8").off("click");
+
+		});
+		
+	 
+	});
+
+	$(".color9").on("click", function(){
+        
+		$(this).removeClass("tile9").addClass('lock');
+		$(".color9").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});	 
+
+		
+		$(".wrong").click(function(){
+	    	    	
+	    	$(".color9").off("click");
+
+		});
+
+	 
+	});
+
+	$(".color10").on("click", function(){
+        
+		$(this).removeClass("tile10").addClass('lock');
+		$(".color10").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});	 
+
+		
+		$(".wrong").click(function(){
+	    	    	
+	    	$(".color10").off("click");
+
+		});
+	 
+	});
+
+	$(".color11").on("click", function(){
+        
+		$(this).removeClass("tile11").addClass('lock');
+		$(".color11").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});	 
+
+		
+		$(".wrong").click(function(){
+	    	    	
+	    	$(".color11").off("click");
+
+		});
+	 
+	});
+
+	$(".color12").on("click", function(){
+        
+		$(this).removeClass("tile12").addClass('lock');
+		$(".color12").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});	 
+
+		
+		$(".wrong").click(function(){
+	    	    	
+	    	$(".color12").off("click");
+
+		});
+	 
+	});
+
+	$(".color13").on("click", function(){
+        
+		$(this).removeClass("tile13").addClass('lock');
+		$(".color13").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});	 
+
+		
+		$(".wrong").click(function(){
+	    	    	
+	    	$(".color13").off("click");
+
+		});
+	 
+	});
+
+	$(".color14").on("click", function(){
+        
+		$(this).removeClass("tile14").addClass('lock');
+		$(".color14").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});	 
+
+		
+		$(".wrong").click(function(){
+	    	    	
+	    	$(".color14").off("click");
+
+		});
+	 
+	});
+
+	$(".color15").on("click", function(){
+        
+		$(this).removeClass("tile15").addClass('lock');
+		$(".color15").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});	 
+
+		
+		$(".wrong").click(function(){
+	    	    	
+	    	$(".color15").off("click");
+
+		});
+	 
+	});
+
+	$(".color16").on("click", function(){
+        
+		$(this).removeClass("tile16").addClass('lock');
+		$(".color16").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});	 
+
+		
+		$(".wrong").click(function(){
+	    	    	
+	    	$(".color16").off("click");
+
+		});
+	 
+	});
+			$(".unlock").click(function(){
+	    	    	
+	    	
+		$("#1").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});
+		$("#2").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});
+		$("#3").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});
+		$("#4").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});
+		$("#5").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});
+		$("#6").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});
+		$("#7").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});
+		$("#8").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});
+		$("#9").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});
+		$("#10").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});
+		$("#11").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});
+		$("#12").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});
+		$("#13").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});
+		$("#14").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});
+		$("#15").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});
+		$("#16").on("click", function(){
+	   	 $(this).addClass('display'); 
+		});
+		});
+	//Unlock all tiles
+	$(".unlock").click(function(){
+		$(".color1").on("click", function(){});
+		$(".color2").on("click", function(){});
+		$(".color3").on("click", function(){});
+		$(".color4").on("click", function(){});
+		$(".color5").on("click", function(){});
+		$(".color6").on("click", function(){});
+		$(".color7").on("click", function(){});
+		$(".color8").on("click", function(){});
+		$(".color9").on("click", function(){});
+		$(".color10").on("click", function(){});
+		$(".color11").on("click", function(){});
+		$(".color12").on("click", function(){});
+		$(".color13").on("click", function(){});
+		$(".color14").on("click", function(){});
+		$(".color15").on("click", function(){});
+		$(".color16").on("click", function(){});	
+	});
+	//Display popup to confirm decision
+	$(".displayAll").click(function(){
+			var r = confirm("Display all Tiles?");
+				if (r == true) {
+				    //$(".row>div").css({"opacity": "0", "font-size": "200%"});
+				    $(".row>div").addClass('fadeEffect'); 
+				} 
+	    		});
+
+	//Intialise Ques div as invisible
+	$(".ques").hide();
+	$(".logo").click(function(){
+		$(".ques").show("slow");
+	});
+	
+}); 
